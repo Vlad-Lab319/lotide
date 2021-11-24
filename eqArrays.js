29d1ae503d49c2c64cfac6748169ad5fc1ee3ab3
Vlad-Lab319 <solo15mag@gmail.com>
2021-11-24 00:28:32 +0000

    Messy array comparing

---
+++ eqArrays.js
@@ -0,0 +1,53 @@
+//Emojis
+const pass = String.fromCodePoint(0x1F600);
+const fale = String.fromCodePoint(0x1F47A);
+
+// Function implementation
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    //console.log('Unequal length', false);
+    return false;
+  } else {
+      let similarElements = true;
+      for (let i = 0; i < arr2.length; i++) {
+        if (arr1[i] === arr2[i]) {
+          //console.log(arr1[i], arr2[i]);
+          //console.log(`${i} different elements index`);
+          
+        } else {
+          similarElements = false;
+          //console.log('scope', similarElements);
+          return similarElements;  
+
+        }
+    }
+    if (similarElements) {
+      //console.log('Equal arrays', similarElements);
+    } 
+    return similarElements;
+    }
+  
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])) // => false
+console.log(eqArrays(1, '1')) // => false
+
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([2], [2]), true); // => should PASS
+assertEqual(eqArrays([2, 5, 1], [2, 4, 6]), true); // => shouldn't PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => shouldn't PASS
