f6445eb4743b71cc9382a7d77e6c27ea412e498e
Vlad-Lab319 <solo15mag@gmail.com>
2021-11-30 00:49:11 +0000

    assertEqual and tailTest simplified

--- assertEqual.js
+++ assertEqual.js
@@ -2,17 +2,19 @@
 const pass = String.fromCodePoint(0x1F600);
 const fale = String.fromCodePoint(0x1F47A);
 
-//Old code just in case for backup
+
 
 // Function implementation
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
+
+//Old code just in case for backup
 
 // //To String version
 // // const assertEqual = function(actual, expected) {
@@ -24,33 +26,33 @@ const fale = String.fromCodePoint(0x1F47A);
 // // };
 
 //Elements comparing
-const assertEqual = function(actual, expected) {
-  let assertion = false;
-  if (actual === undefined) {
-    assertion = false;
-    console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
-    return;
-  }
-  if (actual === expected) {
-    console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
-    // return;
-  } else if (actual.length !== expected.length) {
-    console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    for (let i = 0; i < expected.length; i++) {
-      if (actual[i] !== expected[i]) {
-        console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
-        break;
-      } else {
-        assertion = true;
-      }
-    }
-  }
-  if (assertion === true) {
-    console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   let assertion = false;
+//   if (actual === undefined) {
+//     assertion = false;
+//     console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
+//     return;
+//   }
+//   if (actual === expected) {
+//     console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
+//     // return;
+//   } else if (actual.length !== expected.length) {
+//     console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
+//   } else {
+//     for (let i = 0; i < expected.length; i++) {
+//       if (actual[i] !== expected[i]) {
+//         console.log(fale, fale, fale, `Assertion Failed: ${actual} !== ${expected}`);
+//         break;
+//       } else {
+//         assertion = true;
+//       }
+//     }
+//   }
+//   if (assertion === true) {
+//     console.log(pass, pass, pass, `Assertion Passed: ${actual} === ${expected}`);
+//   }
+// };
 
-console.log(assertEqual(1, 2));
+console.log(assertEqual(1, 1));
 
 module.exports = assertEqual;
--- test/tailTest.js
+++ test/tailTest.js
@@ -3,17 +3,24 @@ const tail = require('../tail');
 
 
 // Test code
-assertEqual(tail([5,6,7,8]), [6,7,8]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail([]), "Hello");
-assertEqual(tail(["Lighthouse"]), "Hello");
-assertEqual(tail([]),[]);
-assertEqual(tail([5,6,7]), [9,7]);
+// assertEqual(tail([5,6,7,8]), [6,7,8]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(tail([]), "Hello");
+// assertEqual(tail(["Lighthouse"]), "Hello");
+// assertEqual(tail([]),[]);
+// assertEqual(tail([5,6,7]), [9,7]);
 
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse"); 
+assertEqual(result[1], "Labs");
 
+const result2 = tail([5, 6, 7, 8]);
+assertEqual(result2.length, 3);
+assertEqual(result2[0], 6); 
+assertEqual(result2[1], 7);
+assertEqual(result2[2], 8);
 
 // // Test Case: Check the original array
 // const words = ["Yo Yo", "Lighthouse", "Labs"];
